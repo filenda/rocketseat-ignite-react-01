d90a15abdd17bfba2e745e5b119651da79b7b45b
Vinicius Filenga <vinifilenga@hotmail.com>
2023-05-09 20:14:34 -0300

    feat: sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,28 @@
-import {Post} from './Post'
-import { Header } from './components/Header'
-import "./global.css"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
+
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
-
   return (
     <>
-    <Header/>
+      <Header />
 
-      <Post 
-        author="Vinicius Filenga" 
-        content="Loren ipsum Loren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsum"/>
-      <Post 
-        author="Gabriel" 
-        content="Loren ipsum Loren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsum"/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Vinicius Filenga"
+            content="Loren ipsum Loren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsum"
+          />
+          <Post
+            author="Gabriel"
+            content="Loren ipsum Loren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsumLoren ipsum"
+          />
+        </main>
+      </div>
     </>
-  )
+  );
 }
-
